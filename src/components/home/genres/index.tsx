564fe886464edd28436c196f1ba6c2765ace83fe
asraful <asrafulislamrayhan2@gmail.com>
2024-06-27 01:23:23 +0600

    added slider to the genres

--- src/components/home/genres/index.tsx
+++ src/components/home/genres/index.tsx
@@ -5,9 +5,16 @@ import Wrapper from "../../shared/Wrapper";
 import Genre from "./Genre";
 import { useGetGenresQuery } from "@/features/genres/genreApi";
 import GenreSkeleton from "./GenreSkeleton";
-import Slider from "./Slider";
+import {
+  CarouselContent,
+  CarouselNext,
+  CarouselPrevious,
+  Carousel,
+  CarouselItem,
+} from "@/components/ui/carousel";
+import { FetchBaseQueryError } from "@reduxjs/toolkit/query";
 const Genres = () => {
-  const { data, isLoading, isError, error } = useGetGenresQuery();
+  const { data, isLoading, isError, error } = useGetGenresQuery(undefined);
   const { results: genres } = data || {};
 
   let content;
@@ -26,12 +33,14 @@ const Genres = () => {
       </>
     );
   } else if (isError) {
-    content = <div className="text-white">{error.status}</div>;
+    content = <div>{(error as FetchBaseQueryError).status}</div>;
   } else if (!isError && !isLoading && genres?.length === 0) {
     content = <div>No genres found</div>;
   } else if (!isError && !isLoading && genres?.length > 0) {
-    content = genres.map((genre: GenreType) => (
-      <Genre key={genre?.id} genre={genre} />
+    content = genres?.map((genre: GenreType) => (
+      <CarouselItem key={genre?.id} className="md:basis-1/2 lg:basis-[12%]">
+        <Genre key={genre?.id} genre={genre} />
+      </CarouselItem>
     ));
   }
 
@@ -42,13 +51,16 @@ const Genres = () => {
           Discover by <span className="text-blue-200">Genre</span>
         </Title>
 
-        <div className="grid grid-cols-3 sm:grid-cols-5 lg:grid-cols-8 gap-5 relative isolate">
-          {content}
-
-          <div className="absolute size-[300px] blur-[100px] bg-blue-300 top-[24%] left-1/3 transform -translate-x-1/2 -translate-y-1/2 -z-10 opacity-65"></div>
-        </div>
-
-        <Slider />
+        <Carousel
+          opts={{
+            align: "start",
+          }}
+          className="w-full"
+        >
+          <CarouselContent className="gap-5">{content}</CarouselContent>
+          <CarouselPrevious className="border-none hover:bg-blue-200 text-black transition-all duration-300" />
+          <CarouselNext className="border-none hover:bg-blue-200 text-black transition-all duration-300" />
+        </Carousel>
       </Wrapper>
     </section>
   );
